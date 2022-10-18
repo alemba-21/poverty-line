4f455bfc7d1147fa53338504e46cfb9a49614739
joseph3559 <scottjoe3559@gmail.com>
2022-10-19 00:26:32 +0300

    Add Sidebar Navigation Menu

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -28,13 +28,14 @@ const Navbar = () => {
       <div onClick={handleNav} className='block md:hidden'>
           {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
-      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-green-600 m-4'>PovertyLine.</h1>
-          <li className='p-4 border-b border-gray-600'>Home</li>
-          <li className='p-4 border-b border-gray-600'>Company</li>
-          <li className='p-4 border-b border-gray-600'>Resources</li>
-          <li className='p-4 border-b border-gray-600'>About</li>
-          <li className='p-4'>Contact</li>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-screen border-r text-center border-r-gray-900 bg-slate-100 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
+        <h1 className='w-full text-xl font-bold text-green-600 m-4'>PovertyLine.</h1>
+        <li className='p-4 hover:text-green-600 border-b border-gray-300 mt-8'>Jobs</li>
+        <li className='p-4 hover:text-green-600 border-b border-gray-300'>About Us</li>
+        <li className='p-4 hover:text-green-600 border-b border-gray-300'>Notifications</li>        
+        <li className='text-black border-2 h-[50px] mt-8 py-2 mx-2 px-4 hover:border-green-600 text-center hover:text-green-600 border-black rounded-full'>Sign Up</li>
+        <li className='text-white border bg-green-600 mt-2  mx-2 py-2 px-4 h-[50px] text-center border-green-600 hover:bg-transparent hover:text-green-600 rounded-full'>Sign In</li>
+      
       </ul>
     </div>
   );
