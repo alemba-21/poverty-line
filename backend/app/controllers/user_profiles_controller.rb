dd8e57e7663a39da8c560679e83874f3d4ed9d7e
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-19 22:16:41 +0300

    user_profile controller actions created

--- backend/app/controllers/user_profiles_controller.rb
+++ backend/app/controllers/user_profiles_controller.rb
@@ -1,2 +1,33 @@
 class UserProfilesController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        user_profiles = UserProfile.all
+        render json: user_profiles
+    end    
+    
+    def create
+        user_profile = UserProfile.create!(user_profile_params)
+        render json:user_profile, status: :created
+    end
+
+    def show
+        user_profile = UserProfile.find(params[:id])
+        render json: user_profile, status: :ok
+    end 
+
+    def update
+        user_profile = UserProfile.find(params[:id])
+        user_profile.update!(user_profile_params)
+        render json: user_profile, status: :accepted
+    end 
+
+    private
+    def user_profile_params
+        params.permit(:username, :firstname, :lastname, :email, :gender, :dob, :national_id, :address, :county, :estate, :user_id)
+    end
+
+    def render_not_found_response
+        render json: { error: "User profile not found" }, status: :not_found
+    end
 end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "seeding demo user_profile"
+
+UserProfile.create(firstname: 'Earnest', lastname: 'Achayo', email: 'earnutech@live.com', gender: 'Male', dob: 12/5/1996, national_id: 297364546, address: '555 Nairobi', county: 'Nakuru', estate: 'Buma', user_id: 2)
+
+puts "Done seeding!"
