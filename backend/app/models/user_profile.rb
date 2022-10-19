89b09770a9176e4f78d73ba98ac5685832838666
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-19 22:29:40 +0300

    validation rules added

--- backend/app/models/user_profile.rb
+++ backend/app/models/user_profile.rb
@@ -1,2 +1,7 @@
 class UserProfile < ApplicationRecord
+
+    validates :first_name,:last_name, :email, :dob, :national_id, :address, :county, presence: true
+    validates :email, uniqueness: true   
+    validates :email, format: {with: URI::MailTo::EMAIL_REGEXP }
+    validates :phone, :presence => true, :numericality => true, :length => { :minimum => 9, :maximum => 13 }
 end
