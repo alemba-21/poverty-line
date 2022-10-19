315de5fb6fbee07de335afe210f4e9a93dbdfb55
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-19 14:42:26 +0300

    User profile resource generated

---
+++ backend/db/migrate/20221019114153_create_user_profiles.rb
@@ -0,0 +1,18 @@
+class CreateUserProfiles < ActiveRecord::Migration[7.0]
+  def change
+    create_table :user_profiles do |t|
+      t.string :firstname
+      t.string :lastname
+      t.string :email
+      t.boolean :gender
+      t.datetime :dob
+      t.integer :national_id
+      t.string :address
+      t.string :county
+      t.string :estate
+      t.integer :user_id
+
+      t.timestamps
+    end
+  end
+end
