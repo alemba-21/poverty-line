7ef610b6f8ed4cb43f88e7b25a85baa06909f0ed
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-20 12:49:06 +0300

    Job resource updated

--- backend/db/migrate/20221020094411_create_jobs.rb
+++ backend/db/migrate/20221020094411_create_jobs.rb
@@ -2,8 +2,8 @@ class CreateJobs < ActiveRecord::Migration[7.0]
   def change
     create_table :jobs do |t|
       t.string :job_title
-      t.string :job_description
-      t.string :required_skills
+      t.text :job_description
+      t.text :required_skills
       t.string :job_link
 
       t.timestamps
