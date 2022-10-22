b17b81126f191836ef744a10c0660b311a72987d
alemba-21 <alembakiragu@gmail.com>
2022-10-22 11:06:30 +0300

    working on search bar

--- frontend/src/components/Job/JobListing_page.jsx
+++ frontend/src/components/Job/JobListing_page.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import {Box, Grid, Button} from '@material-ui/core'
 
 const JobListing_page = () => {
   return (
-    <div>JobListing_page</div>
+    <Box class="py-20 bg-green-600">
+        <Grid container class="flex justify-center">
+            <Grid item xs={10}>
+                <Box class="flex items-center flex justify-between">
+                    <h1 class="text-5xl">Open Job Positions</h1>
+                    <Button class="normal-case text-2xl border-4 border-indigo-100/100" >Post a Job</Button>
+                </Box>
+            </Grid>
+        </Grid>
+    </Box>
   )
 }
 
