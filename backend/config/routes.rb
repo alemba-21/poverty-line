c1e989f72f2c95639cb9a78d7dcd4d9761d6dc59
Bryan Bosire <bryanbosire@gmail.com>
2022-10-23 15:11:50 +0300

    created users routebwith method post and get to login

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   resources :jobs
   resources :user_profiles
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+ 
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'auth#create'
+      get '/profile', to: 'users#profile'
+    end
+  end
 end
