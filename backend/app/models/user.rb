798d5b94384bac4773a8cf937e97036a8b1fee73
Bryan Bosire <bryanbosire@gmail.com>
2022-10-23 15:33:41 +0300

    added validates_format_of email with /@/

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -2,9 +2,8 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, presence: true, uniqueness: true
-    # validates :password, presence: true, length: { minimum: 6 }
-    # validates email has @ and .
-    # validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    
-    # has_many :user_profiles
+    validates :password, presence: true, length: { minimum: 6 }
+    validates_format_of :email, with: /@/
+
+    has_many :user_profiles
 end
