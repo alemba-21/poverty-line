91051d505311803c8bf66e9cf2a721adb27ea6a9
Snaida.Kabalika <snaida.kabalika@student.moringaschool.com>
2022-10-24 13:20:05 +0300

    add input type to form

--- frontend/src/components/Jobs.jsx
+++ frontend/src/components/Jobs.jsx
@@ -5,7 +5,23 @@ const Jobs = () => {
     <div className='card'>
         <div className='box'>
           <form>
-            
+            <div className='title'>
+              <h1>Job Title</h1>
+            </div>
+            <div className='des'>
+              <h1>Job Description</h1>
+              <input
+                type='textarea'
+                placeholder='Job Description'
+              />  
+            </div>
+            <div className='skills'>
+              <h1>Required Skils</h1>
+              <input
+                type='text'
+                placeholder='Required Skills'
+              />  
+            </div>
           </form>
         </div>
     </div>
