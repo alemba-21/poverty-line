087bbfae941b90d99c30088cdc41bd21943736c6
alemba-21 <alembakiragu@gmail.com>
2022-10-24 16:37:07 +0300

    working on routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,16 @@ import JobDetails from "./Links/JobDetails";
 import Apply from "./Links/Application";
 import Home from "./Links/Home"
 import Profile from "./Links/Profile";
+import SignIn from "./Links/SignIn";
+import SignedUp from "./Links/SignedUp";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signed" element={<SignedUp />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/profile" element={<Profile/>} />
         <Route path="/create" element={<Profile />} />
         <Route path="/Jobs" element={<Jobs/>}/>
         <Route path="/Details" element={<JobDetails/>}/>
---
+++ frontend/src/Links/SignedUp.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Registration from '../components/Registration'
+
+
+const SignedUp = () => {
+  return (
+    <div>
+        <Registration />
+    </div>
+  )
+}
+
+export default SignedUp
