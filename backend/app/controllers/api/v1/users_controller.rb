39c9be5f6d613d6ec3a69d8281bf2ebf70a965b4
Bryan Bosire <bryanbosire@gmail.com>
2022-10-24 10:22:58 +0300

    created profilr and registration for user and authorized

--- backend/app/controllers/api/v1/users_controller.rb
+++ backend/app/controllers/api/v1/users_controller.rb
@@ -1,9 +1,15 @@
 class Api::V1::UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def profile
+        render json: { user: UserSerializer.new(current_user) }, status: :accepted
+    end
 
     def create 
-        @user = User.new(user_params)
+        @user = User.create(user_params)
         if @user.valid?
-            render json: {user: UserSerializer.new(@user)}, status: :created
+            @token = encode_token({ user_id: @user.id })
+            render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
         else
             render json: {error: "failed to create user"}, status: :unprocessable_entity
         end
@@ -12,6 +18,6 @@ class Api::V1::UsersController < ApplicationController
     private
 
     def user_params
-        params.permit(:email, :password)
+        params.require(:user).permit(:email, :password)
     end
 end
