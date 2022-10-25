094ee100d4373962309a910ab54553c55490571d
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-25 20:07:03 +0300

    added update, delete  actions plus private methods for code cleaning for user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -21,4 +21,29 @@ class UsersController < ApplicationController
         end
     end
 
+    def update
+    unless @user.update(user_params)
+        render json: { errors: @user.errors.full_messages },
+                status: :unprocessable_entity
+    end
+    end
+
+    def destroy
+    @user.destroy
+    end
+
+    private
+
+    def find_user
+        @user = User.find_by_username!(params[:_username])
+        rescue ActiveRecord::RecordNotFound
+          render json: { errors: 'User not found' }, status: :not_found
+    end
+
+    def user_params
+    params.permit(
+        :avatar, :name, :username, :email, :password, :password_confirmation
+    )
+    end
+
 end
