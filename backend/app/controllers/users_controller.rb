a061096943be00349b09b56e0663e23a090f2df3
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-25 20:03:26 +0300

    added index, show and create actions for user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,2 +1,24 @@
 class UsersController < ApplicationController
+    before_action :authorize_request, except: :create
+    before_action :find_user, except: %i[create index]
+
+    def index
+        @users = User.all
+        render json: @users, status: :ok
+    end
+
+    def show
+        render json: @user, status: :ok
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: { errors: @user.errors.full_messages },
+                 status: :unprocessable_entity
+        end
+    end
+
 end
