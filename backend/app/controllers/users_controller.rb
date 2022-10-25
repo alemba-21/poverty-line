2cd0f5157602e63fe663d0a30f7956e4fd75ddbd
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-25 20:19:34 +0300

    error bugging in user controller

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -41,9 +41,7 @@ class UsersController < ApplicationController
     end
 
     def user_params
-    params.permit(
-        :avatar, :name, :username, :email, :password, :password_confirmation
-    )
+    params.permit(:name, :username, :email, :password, :password_confirmation)
     end
 
 end
--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { minimum: 6 }, if: -> { new_record? || !password.nil? } 
     validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :username, presence: true, uniqueness: true
 
     has_many :user_profiles
 end
