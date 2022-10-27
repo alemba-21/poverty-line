0423d7637444ecea021be33f117043aba737ea01
joseph3559 <scottjoe3559@gmail.com>
2022-10-27 11:55:11 +0300

    Create Props for message component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import {Routes, Route} from "react-router-dom";
 import Jobs from "./Links/Jobs";
-import JobDetails from "./Links/JobDetails";
 import Home from "./Links/Home"
 import Profile from "./Links/Profile";
 import SignIn from "./Links/SignIn";
@@ -8,6 +7,7 @@ import SignedUp from "./Links/SignedUp";
 import About from "./Links/About";
 import ProfView from './Links/ProfView'
 import React from "react";
+import Message from "./components/Message";
 
 
 function App() {
@@ -16,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/View" element={<ProfView />} />
         <Route path="/About" element={<About />}/>
+        <Route path="/Chat" element={<Message />} />
         <Route path="/signed" element={<SignedUp />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/profile" element={<Profile/>} />
