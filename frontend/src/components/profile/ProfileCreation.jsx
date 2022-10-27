68a0904a7a50bdfa0a192c94acf00242b0fdcc7b
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-27 16:25:25 +0300

    fixed experienced_date input under profileCreation

--- frontend/src/components/profile/ProfileCreation.jsx
+++ frontend/src/components/profile/ProfileCreation.jsx
@@ -18,7 +18,7 @@ const ProfileCreation = () => {
     county: "",
     estate: "",
     career_summary: "",
-    experienceDate: "",
+    experience_date: "",
     experience: "",
     education_date: "",
     education: "",
@@ -240,8 +240,8 @@ const ProfileCreation = () => {
               <input className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-password"
                 type="date"
-                name="date"
-                value={formData?.date}
+                name="experience_date"
+                value={formData?.experience_date}
                   onChange={handleChange}
               />
         </div>
