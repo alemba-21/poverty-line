e069c3ba06f169c5818a9037650b7ace6fe80033
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-28 20:32:37 +0300

    job_application table migration

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,9 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_25_075143) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_28_165455) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "job_applications", force: :cascade do |t|
+    t.string "cover_letter"
+    t.string "attachment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "jobs", force: :cascade do |t|
     t.string "job_title"
