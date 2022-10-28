a766ef46d57cf38f88d9953e66b6774e07ba0db9
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-28 19:55:57 +0300

    job application resource generation

---
+++ backend/app/controllers/job_applications_controller.rb
@@ -0,0 +1,3 @@
+class JobApplicationsController < ApplicationController
+    
+end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :job_applications
   resources :jobs
   resources :user_profiles
  
