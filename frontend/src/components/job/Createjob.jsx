cbda9976b76d4a0b50459ebe3e310c5bf6a81986
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-10-30 15:07:53 +0300

    fixed jobCreation component button for navigation to job listing after submission of profile data

--- frontend/src/components/job/Createjob.jsx
+++ frontend/src/components/job/Createjob.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from '../../api/axios'
 import './createjob.css'
-import {Link} from 'react-router-dom'
+import {Navigate} from 'react-router-dom'
 
 const Createjob = () => {
   const selectedTags = tags => {
 		console.log(tags);
   };
+
+  
 
   return (
 		<div className="App">
@@ -26,7 +28,9 @@ const Createjob = () => {
         event.target.value = "";
       };
     } 
-    
+
+  
+  const [validated, setValidated] = useState(false);  
   const [formData, setFormData] = useState({
     job_title: "",
     company_name: "",
@@ -52,94 +56,99 @@ const Createjob = () => {
     axios.post('/jobs', formData)
       .then((response) => {
         console.log(response)
+        setValidated(true);
       })
   }
 
-  return (
-    <div className='card'>
-        <div className='box'>
-          <form className='entry-form'>
-            <div className='title'>
-              <h1>Job Title</h1>
-            <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              id="grid-password"
-              type="text"
-              name="job_title"
-              value={formData?.job_title}
-              onChange={handleChange}
-            />
-             
-          </div>
-          <div className='title'>
-              <h1>Company name</h1>
-            <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              id="grid-password"
-              type="text"
-              name="company_name"
-              value={formData?.company_name}
-              onChange={handleChange}
-            />
-             
-            </div>
-            <div className='des'>
-              <h1>Job Description</h1>
-            <textarea class="block p-2.5 w-full text-sm text-gray-900 h-36 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              id="grid-password"
-              type="text"
-              name="job_description"
-              value={formData?.job_description}
-              onChange={handleChange}
-            />
-          </div>
-
-          <div className='title'>
-                <h1>Job Type</h1>
+    return (
+      <>
+        {validated ? (
+        <Navigate to= "/jobs"></Navigate>
+        ) : (
+          <div className='card'>
+          <div className='box'>
+            <form className='entry-form'>
+              <div className='title'>
+                <h1>Job Title</h1>
               <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 id="grid-password"
                 type="text"
-                name="job_type"
-                value={formData?.job_type}
+                name="job_title"
+                value={formData?.job_title}
                 onChange={handleChange}
-            />
-            
-          </div>
-          
-            <div className='skills'>
-              <h1>Required Skills</h1>
-            <textarea onKeyUp={ event => event.key === "Enter" ? addTags(event) : null } class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              id="grid-password"
-              type="text"
-              placeholder="Required Skills"
-              name="required_skills"
-              value={formData?.required_skills}
-              onChange={handleChange}
-            />
-                            
-              <ul id="tags">
-            {tags.map((tag, index) => (
-              <li key={index} className="tag">
-                <span className='tag-title'>{tag}</span>
-                <span className='tag-close-icon'
-                  onClick={() => removeTags(index)}
-                >
-                  x
-                </span>
-              </li>
-            ))}
-			  </ul>
-            
+              />
+               
+            </div>
+            <div className='title'>
+                <h1>Company name</h1>
+              <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="grid-password"
+                type="text"
+                name="company_name"
+                value={formData?.company_name}
+                onChange={handleChange}
+              />
+               
+              </div>
+              <div className='des'>
+                <h1>Job Description</h1>
+              <textarea class="block p-2.5 w-full text-sm text-gray-900 h-36 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="grid-password"
+                type="text"
+                name="job_description"
+                value={formData?.job_description}
+                onChange={handleChange}
+              />
+            </div>
+  
+            <div className='title'>
+                  <h1>Job Type</h1>
+                <textarea class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  id="grid-password"
+                  type="text"
+                  name="job_type"
+                  value={formData?.job_type}
+                  onChange={handleChange}
+              />
               
             </div>
-          <div className="flex space-between m-auto gap-2 px-4">
-            <Link to="/jobs">
+            
+              <div className='skills'>
+                <h1>Required Skills</h1>
+              <textarea onKeyUp={ event => event.key === "Enter" ? addTags(event) : null } class="block p-2.5 w-full text-sm text-gray-900 bg-gray-200 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                id="grid-password"
+                type="text"
+                placeholder="Required Skills"
+                name="required_skills"
+                value={formData?.required_skills}
+                onChange={handleChange}
+              />
+                              
+                <ul id="tags">
+              {tags.map((tag, index) => (
+                <li key={index} className="tag">
+                  <span className='tag-title'>{tag}</span>
+                  <span className='tag-close-icon'
+                    onClick={() => removeTags(index)}
+                  >
+                    x
+                  </span>
+                </li>
+              ))}
+          </ul>
+              
+                
+              </div>
+            <div className="flex space-between m-auto gap-2 px-4">
               <button onClick={handleSubmit} className='bg-green-600 w-[200px] rounded-full font-med ium my-6  py-3 text-black'>Submit Post</button>
-            </Link>
-          
-          <button className='bg-slate-200 border-2 border-red-600 hover:text-red-600 w-[200px] rounded-full font-medium my-6  py-3 text-black'>Cancel</button>
-        </div>
-      </form>
+              <button className='bg-slate-200 border-2 border-red-600 hover:text-red-600 w-[200px] rounded-full font-medium my-6  py-3 text-black'>Cancel</button>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
+        )}
+      </>
+    
   )
 }
 
