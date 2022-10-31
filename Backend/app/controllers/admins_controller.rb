6dc46d8536f1c7722aea039341b53ef96995746f
Earnest Achayo <earnest.achayo@student.moringaschool.com>
2022-11-01 00:56:38 +0300

    added index and create actions for admin controller

--- Backend/app/controllers/admins_controller.rb
+++ Backend/app/controllers/admins_controller.rb
@@ -1,2 +1,19 @@
 class AdminsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        admins = Admin.all
+        render json: admins
+    end
+
+    def show
+        @user = Admin.find(params[:id])
+        render json: @user
+    end
+
+    private  
+    
+    def render_not_found_response
+        render json: { error: "Admin not found" }, status: :not_found
+    end
 end
