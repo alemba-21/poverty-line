c6c0c22a44dbd12920117d86e7ffb3306de84c77
Tim254 <tim.mailu@gmail.com>
2022-11-03 13:47:00 +0300

    add admin dashboard

--- Frontend/src/components/Admindashboard.jsx
+++ Frontend/src/components/Admindashboard.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ManageJobs from "./admin/ManageJobs";
 
 export default function AdminDashboard() {
   return (
@@ -7,7 +8,7 @@ export default function AdminDashboard() {
       <div className="flex flex-col h-screen p-3 shadow w-60 ">
         <div className="space-y-9 bg-white h-screen rounded-lg">
           <div className="flex justify-center mt-2">
-            <span className="text-3xl font-bold">My Profile</span>
+            <span className="text-3xl font-bold">Admin</span>
           </div>
           <div className="flex-1">
             <ul className="pt-2 pb-4 space-y-1 text-sm">
@@ -55,7 +56,7 @@ export default function AdminDashboard() {
                   <span className="text-2xl">Jobs</span>
                 </a>
               </li>
-              <li className="rounded-sm">
+              {/* <li className="rounded-sm">
                 <a
                   href="Applications"
                   className="flex items-center p-2 space-x-3 rounded-md"
@@ -74,11 +75,11 @@ export default function AdminDashboard() {
                       d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
                     />
                   </svg>
-                  <Link to='/post'>
-                  <span className="text-2xl">Post Job</span>
+                  <Link to="/post">
+                    <span className="text-2xl">Post Job</span>
                   </Link>
                 </a>
-              </li>
+              </li> */}
               <li className="rounded-sm">
                 <a
                   href="inbox"
@@ -101,8 +102,59 @@ export default function AdminDashboard() {
                        1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155"
                     />
                   </svg>
-                  <Link to='/chats'>
-                  <span className="text-2xl">Inbox</span>
+                  <Link to="/chats">
+                    <span className="text-2xl">Inbox</span>
+                  </Link>
+                </a>
+              </li>
+
+              <li className="rounded-sm object-bottom">
+                <a
+                  href="/adminlogin"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="green"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M18 18.72a9.094 9.094 0 003.741-.479 3 3 0 00-4.682-2.72m.94 3.198l.001.031c0 .225-.012.447-.037.666A11.944 11.944 0 0112 21c-2.17 0-4.207-.576-5.963-1.584A6.062 6.062 0 016 18.719m12 0a5.971 5.971 0 00-.941-3.197m0 0A5.995 5.995 0 0012 12.75a5.995 5.995 0 00-5.058 2.772m0 0a3 3 0 00-4.681 2.72 8.986 8.986 0 003.74.477m.94-3.197a5.971 5.971 0 00-.94 3.197M15 6.75a3 3 0 11-6 0 3 3 0 016 0zm6 3a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0zm-13.5 0a2.25 2.25 0 11-4.5 0 2.25 2.25 0 014.5 0z"
+                    />
+                  </svg>
+
+                  <Link to="/usersdata">
+                    <span className="text-2xl ">Users</span>
+                  </Link>
+                </a>
+              </li>
+              <li className="rounded-sm object-bottom">
+                <a
+                  href="/adminlogin"
+                  className="flex items-center p-2 space-x-3 rounded-md"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={2}
+                    stroke="green"
+                    className="w-6 h-6"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M3.375 19.5h17.25m-17.25 0a1.125 1.125 0 01-1.125-1.125M3.375 19.5h7.5c.621 0 1.125-.504 1.125-1.125m-9.75 0V5.625m0 12.75v-1.5c0-.621.504-1.125 1.125-1.125m18.375 2.625V5.625m0 12.75c0 .621-.504 1.125-1.125 1.125m1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125m0 3.75h-7.5A1.125 1.125 0 0112 18.375m9.75-12.75c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125m19.5 0v1.5c0 .621-.504 1.125-1.125 1.125M2.25 5.625v1.5c0 .621.504 1.125 1.125 1.125m0 0h17.25m-17.25 0h7.5c.621 0 1.125.504 1.125 1.125M3.375 8.25c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125m17.25-3.75h-7.5c-.621 0-1.125.504-1.125 1.125m8.625-1.125c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125M12 10.875v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 10.875c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125M13.125 12h7.5m-7.5 0c-.621 0-1.125.504-1.125 1.125M20.625 12c.621 0 1.125.504 1.125 1.125v1.5c0 .621-.504 1.125-1.125 1.125m-17.25 0h7.5M12 14.625v-1.5m0 1.5c0 .621-.504 1.125-1.125 1.125M12 14.625c0 .621.504 1.125 1.125 1.125m-2.25 0c.621 0 1.125.504 1.125 1.125m0 1.5v-1.5m0 0c0-.621.504-1.125 1.125-1.125m0 0h7.5"
+                    />
+                  </svg>
+
+                  <Link to="/usersdata">
+                    <span className="text-2xl ">Profiles</span>
                   </Link>
                 </a>
               </li>
@@ -125,46 +177,38 @@ export default function AdminDashboard() {
                       d="M11 16l-4-4m0 0l4-4m-4 4h14m-5 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h7a3 3 0 013 3v1"
                     />
                   </svg>
-                  <Link to="/adminlogin">
-                        <span className="text-2xl ">Logout</span>
+                  <Link to="/usersprofiles">
+                    <span className="text-2xl ">Logout</span>
                   </Link>
                 </a>
               </li>
-              <li>
-                <Link to="/usersdata" >
-                    <span className="text-2xl ">Users</span>
-                  </Link>
-                </li>
-
-                <li>
-                  <Link to="/usersprofiles">
-                  <span className="text-2xl ">Profiles</span>
-                  </Link>                    
-                </li>              
+     
             </ul>
           </div>
         </div>
       </div>
       <div className="container mx-auto mt-4 px-5">
-        <div className="w-full px-4 py-4 bg-white rounded-lg shadow mb-4 flex justify-end">
-          Notifications
+        <div className="w-full px-4 py-4 bg-white rounded-lg shadow mb-4 flex justify-start">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
-            viewBox="0 0 22 22"
+            viewBox="0 0 24 24"
             strokeWidth={2}
             stroke="green"
-            className="w-7 h-7  "
+            className="w-6 h-6"
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
+              d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
             />
           </svg>
-        </div>
 
-        <div className="grid gap-4 grid-cols-2 ">
+          <h3 className="text-2xl font-medium ml-2 ">Manage Jobs</h3>
+        </div>
+        <ManageJobs/>
+
+        {/* <div className="grid gap-4 grid-cols-2 ">
           <div className="w-full px-20 py-20 bg-white rounded-lg shadow transform motion-safe:hover:scale-105 text-center">
             <div className="text-5xl font-medium text-black-500 truncate ">
               Active Jobs
@@ -189,37 +233,36 @@ export default function AdminDashboard() {
             </div>
             <div className="mt-1 text-4xl font-semibold text-red-600">2</div>
           </div>
-        </div>
+        </div> */}
         <div className="w-full px-8 py-8 bg-white rounded-lg shadow mx-auto mt-4 flex justify-center space-x-40 ">
           <div>
             <h1 className="text-2xl font-medium text-black-500 truncate">
-              Start Posting jobs and say bye to Poverty
+              Start Posting jobs to alleviate Poverty
             </h1>
             <span>
-              Our Partners Are Ranked as the Leading Employers in the Region
+              We pride ourselves as the Leading Employers in the Region
             </span>
           </div>
           <div className="flex justify-end shadow-2xl ">
             <Link to="/post">
-            <button className="bg-green-600 text-3xl text-black py-2 px-3 flex rounded-full">
-              Post a Job
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={3}
-                stroke="currentColor"
-                className="w-10 h-18"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M8.25 4.5l7.5 7.5-7.5 7.5"
-                />
-              </svg>
-            </button>
+              <button className="bg-green-600 text-3xl text-black py-2 px-3 flex rounded-full">
+                Post a Job
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={3}
+                  stroke="currentColor"
+                  className="w-10 h-18"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                  />
+                </svg>
+              </button>
             </Link>
-     
           </div>
         </div>
       </div>
